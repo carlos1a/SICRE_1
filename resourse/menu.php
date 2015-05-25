06d66a43f9e759535c63b6bc67389154bfd4fc62
carlos <cg_47@hotmail.com>
2015-05-24 20:07:05 -0430

    ...

--- resourse/menu.php
+++ resourse/menu.php
@@ -22,7 +22,7 @@
         </a>
         <ul class="dropdown-menu">
           <li><a href="registrar_usuario.php">Registrar Usuario</a></li>
-          <li><a href="#">Consultar Usuarios</a></li>
+          <li><a href="consultar_usuario.php">Consultar Usuarios</a></li>
           <li><a href="#">Modificar Usuario</a></li>
           <li><a href="#">Bloquear Usuario</a></li>
         </ul>
