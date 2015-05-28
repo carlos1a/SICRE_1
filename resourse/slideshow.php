8f2570d0d50b5254629311238263290e7d28964a
carlos <cg_47@hotmail.com>
2015-05-28 19:05:44 -0430

    ....

--- resourse/slideshow.php
+++ resourse/slideshow.php
@@ -9,13 +9,13 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="../css/images/sala.jpg" width="1600px"  alt="...">
+      <img src="../css/images/logofp.jpg" width="1400px"  alt="...">
       <div class="carousel-caption">
 
       </div>
     </div>
     <div class="item">
-      <img src="../css/images/sala.jpg" width="1600px" alt="...">
+      <img src="../css/images/logofp.jpg" width="1400px" alt="...">
       <div class="carousel-caption">
 
       </div>
